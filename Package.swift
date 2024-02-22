6c96ecdbed1186d301c429ca6b29de5ac3412916
Yazan Qaisi <yazan.h.q@hotmail.com>
2024-02-22 13:25:19 +0300

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "V8-Engine",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "V8-Engine",
+            targets: ["V8-Engine"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "V8-Engine"),
+        .testTarget(
+            name: "V8-EngineTests",
+            dependencies: ["V8-Engine"]),
+    ]
+)
---
+++ Tests/V8-EngineTests/V8_EngineTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import V8_Engine
+
+final class V8_EngineTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
